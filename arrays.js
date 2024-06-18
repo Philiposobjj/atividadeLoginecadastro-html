9e571ac8fff6aef7785f571a8a8c178a10a82d41
Philiposobjj <165970578+Philiposobjj@users.noreply.github.com>
2024-06-18 19:29:01 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,24 @@
+let usuario = []; 
+let senha = []; 
+function cadastro() {  
+  let usuario = document.getElementById("Usuario").value; 
+  let senha = document.getElementById("Senha").value; 
+  if (usuario != "" && senha != 0) {
+    if (usuario.indexOf(usuario) == -1) {
+      
+    }
+} window.location.href="about.html"
+}
+function login() {  
+    let usuario = document.getElementById("Usuario").value; 
+    let senha = document.getElementById("Senha").value; 
+    if (usuario != "" && senha != 0) {
+      if (usuario.indexOf(usuario) == -1) {
+        
+      }
+  } window.location.href="about.html"
+  }
+  
+    function desconectar(){
+        window.location.href='index.html'
+    }
